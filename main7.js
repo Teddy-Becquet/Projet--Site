62b878bf397a8bac27d732596d3e084428f70241
Your Name <you@example.com>
2024-11-29 10:31:37 +0100

    exo7

--- main7.js
+++ main7.js
@@ -1,7 +1,15 @@
+function addition(a, b) {
+    return a + b;
+}
+
 var demande = parseInt(prompt("Entrez un nombre")); 
+
 var resultat = 0;
 
+
 for (var i = 1; i <= demande; i++) {
-    resultat += i; 
+    
+    resultat = addition(resultat, i); 
 }
-alert( resultat);
+
+alert(resultat);
